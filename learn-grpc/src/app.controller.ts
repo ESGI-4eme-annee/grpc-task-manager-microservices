b2954961979ea05678ef65c56f96562a60917256
Nicolas CAPELLA <colas.capel73@gmail.com>
2023-04-26 11:49:44 +0200

    :tada: proto

---
+++ learn-grpc/src/app.controller.ts
@@ -0,0 +1,16 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+import { GrpcMethod } from '@nestjs/microservices';
+
+@Controller()
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  @GrpcMethod('HelloService')
+  sayHello(req) {
+    const name = req.name;
+    return {
+      message: `Hello ${name}`,
+    };
+  }
+}
